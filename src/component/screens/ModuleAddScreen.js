d000b1601a9bb73351eae69c49cc9828b5747abc
samia07farid <67553668+samia07farid@users.noreply.github.com>
2024-10-15 12:12:49 +0100

    completed second video changes (#1)

---
+++ src/component/screens/ModuleAddScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text} from 'react-native';
+import Screen from '../layout/screen';
+
+export const ModuleAddScreen = () => {
+  return (
+    <Screen>
+      <Text>Add</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleAddScreen;
---
+++ src/component/screens/ModuleModifyScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text} from 'react-native';
+import Screen from '../layout/screen';
+
+export const ModuleModifyScreen = () => {
+  return (
+    <Screen>
+      <Text>Modify</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleModifyScreen;
